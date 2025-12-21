cba401b1eaff29b9bf9a54c38549be9dc14adfc1
Franciss <franlorlaz@gmail.com>
2025-12-21 21:20:32 +0100

    Refactor existing CRUD

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,7 +1,7 @@
 from sqlmodel import Session, create_engine, select
 
-from app import crud
 from app.core.config import settings
+from app.crud.user import create_user
 from app.models import User
 from app.schemas.user.user_creation import UserCreate
 
@@ -31,4 +31,4 @@ def init_db(session: Session) -> None:
             password=settings.FIRST_SUPERUSER_PASSWORD,
             is_superuser=True,
         )
-        user = crud.create_user(session=session, user_create=user_in)
+        user = create_user(session=session, user_create=user_in)
